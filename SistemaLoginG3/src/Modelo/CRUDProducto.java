89e4c631fc6774ac02aed3867cde927fd073f21f
Jean Carlo Martinez Vizueth <24300603@uttt.edu.mx>
2025-08-17 13:09:25 -0600

    Modifique CRUDPruducto

--- SistemaLoginG3/src/Modelo/CRUDProducto.java
+++ SistemaLoginG3/src/Modelo/CRUDProducto.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 
 public interface CRUDProducto {
     public boolean insertar();
+
     public ArrayList buscar();
+
     public boolean buscarPorId(int id);
+
     public boolean modificar(int id);
+
     public boolean eliminar(int id);
 }
